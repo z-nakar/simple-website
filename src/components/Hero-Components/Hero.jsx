a6501eec6f50bba992cc6300bcc9b39c9d18cfbf
@CodeWithZaii <xairelnakar01@gmail.com>
2024-11-07 19:04:05 +0800

    import

--- src/components/Hero-Components/Hero.jsx
+++ src/components/Hero-Components/Hero.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../Hero-Components/Hero.css'
+import btn_arrow from '../Background-Components/arrow_btn.png'
+import btn_pause from '../Background-Components/pause-icon.png'
+import btn_play from '../Background-Components/play-icon.png'
+
 
 const Hero = ({heroData, setHeroCount, heroCount, setPlayStatus, playStatus}) => {
   return (
